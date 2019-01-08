237c7bab41d21637abc7d2a0a40a6a4ce7a56409
Horst Wang <horst.wang@emc.com>
2019-01-08 15:07:23 +0000

    Add a variable KUBE_VER at the beginning of install-kubeadm.sh for capable of changing target kubeadm and kubernets version ...

--- docker_script/install-kubeadm.sh
+++ docker_script/install-kubeadm.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 
+KUBE_VER=1.12.1-00
+
 sudo apt-get update && sudo apt-get install -y apt curl
 wget http://packages.cloud.google.com/apt/doc/apt-key.gpg --no-check-certificate && cat ./apt-key.gpg | sudo apt-key add -
 sudo /bin/bash -c "cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
@@ -7,5 +9,5 @@ deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF"
 sudo cp ./disable-apt-ssl /etc/apt/apt.conf.d/
 sudo apt-get update
-sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-get install -y kubelet=$KUBE_VER kubeadm=$KUBE_VER kubectl=$KUBE_VER --allow-change-held-packages
 sudo apt-mark hold kubelet kubeadm kubectl
